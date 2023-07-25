6b8d1037e24880c8d985564feb76acd52c6ee418
Blue <140518252+BlueCode23@users.noreply.github.com>
2023-07-25 15:10:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+#we get user's inputs here
+activity = input("What is your channel's activity : ")
+color = input("What is your favorite color : ")
+
+#we print the computed result here
+print("Super ! Your generatedd youtube channel's name is : " + color + activity)
